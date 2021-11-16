c7df85fc5fd09671dc434a67b43844fa9a106ba2
MacCormick <jmac@dickinson.edu>
2021-11-15 19:46:39 -0500

    update java files

--- resources/class18/ComputerScientist.java
+++ resources/class18/ComputerScientist.java
@@ -1,24 +1,50 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 
+/**
+ * The ComputerScientist class is a simple class used to demonstrate hash
+ * tables. Each instance of the class stores the name and date of birth of
+ * a computer scientist.
+ * 
+ * @author jmac
+ */
 public class ComputerScientist {
+
+	// The date on which the computer scientist was born.
 	private LocalDate birthDate;
+
+	// The name of the computer scientist, e.g. "Ada Lovelace"
 	private String name;
 
+	/**
+	 * Construct a new ComputerScientist object storing the name and date
+	 * of birth of a computer scientist.
+	 * 
+	 * @param name      The name of the computer scientist, e.g. "Ada
+	 *                  Lovelace"
+	 * @param birthDate The date of birth of the computer scientist,
+	 *                  expressed as a string in the format YYYY-MM-DD,
+	 *                  e.g. "1972-06-30"
+	 */
 	public ComputerScientist(String name, String birthDate) {
+		// This will throw a run-time exception if the string is not
+		// correctly formatted as the date, but for simplicity we choose
+		// not to deal with that here.
 		this.birthDate = LocalDate.parse(birthDate);
+
 		this.name = name;
 	}
 
 	/**
-	 * @return the birthDate
+	 * @return the date on which the computer scientist was born
 	 */
 	LocalDate getBirthDate() {
 		return birthDate;
 	}
 
 	/**
-	 * @return the name
+	 * @return the name of the computer scientist
 	 */
 	String getName() {
 		return name;
@@ -29,28 +55,11 @@ public class ComputerScientist {
 		return name + " (" + birthDate + ")";
 	}
 
-	@Override
-	public int hashCode() {
-		return birthDate.hashCode();
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ComputerScientist other = (ComputerScientist) obj;
-		if (birthDate == null) {
-			if (other.birthDate != null)
-				return false;
-		} else if (!birthDate.equals(other.birthDate))
-			return false;
-		return true;
-	}
-
+	/**
+	 * @return An Arraylist containing ComputerScientist objects that
+	 *         represent some famous computer scientists, including two
+	 *         Dickinson alums.
+	 */
 	public static ArrayList<ComputerScientist> someCompScis() {
 		String[][] values = { { "Ada Lovelace", "1815-12-10" },
 				{ "Grace Hopper", "1906-12-09" },
@@ -70,16 +79,55 @@ public class ComputerScientist {
 	}
 
 	public static void main(String[] args) {
+
+		////////////////////////////////////////////////////////////////////
+		///// Print the list of famous computer scientists
+		////////////////////////////////////////////////////////////////////
 		System.out.println("Here are the computer scientists:");
 		ArrayList<ComputerScientist> compScis = someCompScis();
 		for (ComputerScientist computerScientist : compScis) {
 			System.out.println(computerScientist);
 		}
 
+		////////////////////////////////////////////////////////////////////
+		///// Print an example of a date and its hash code.
+		////////////////////////////////////////////////////////////////////
 		System.out.println();
-		System.out.println("Here is an example of a date and its hashcode:");
+		System.out
+				.println("Here is an example of a date and its hashcode:");
 		LocalDate d = LocalDate.of(1938, 1, 10);
 		System.out.println(d + ", hash: " + d.hashCode() + ", hash%9: "
 				+ d.hashCode() % 9 + ", hash%18: " + d.hashCode() % 18);
+
+		////////////////////////////////////////////////////////////////////
+		///// Construct a hash table of computer scientists using key-value
+		///// pairs in which the key is the birthdate and the value is the
+		///// name of the computer scientist.
+		////////////////////////////////////////////////////////////////////
+		HashMap<LocalDate, String> compSciMap = new HashMap<>();
+		for (ComputerScientist computerScientist : compScis) {
+			compSciMap.put(computerScientist.getBirthDate(),
+					computerScientist.getName());
+		}
+
+		////////////////////////////////////////////////////////////////////
+		//// Demonstrate how to look up the name of computer scientist
+		//// given the date of birth.
+		////////////////////////////////////////////////////////////////////
+		System.out.println();
+		System.out.println("Now trying some lookups of specific dates...");
+		LocalDate[] datesToLookup = { LocalDate.of(1912, 6, 23),
+				LocalDate.of(1975, 8, 15), LocalDate.of(1943, 8, 1),
+				LocalDate.of(1959, 1, 9), LocalDate.of(1943, 8, 19),
+				LocalDate.of(1964, 3, 23), };
+		for (LocalDate date : datesToLookup) {
+			String name = compSciMap.get(date);
+			if (name == null) {
+				System.out.println(
+						"No known computer scientist was born on " + date);
+			} else {
+				System.out.println(name + " was born on " + date);
+			}
+		}
 	}
 }
--- resources/class18/HashCodes.java
+++ resources/class18/HashCodes.java
@@ -1,6 +1,21 @@
-
+/**
+ * This program is a simple demonstration of the hash codes computed by
+ * Java for several different types of objects.
+ * 
+ * @author jmac
+ */
 public class HashCodes {
 
+	/**
+	 * Print hash code information about an array of objects. The computed
+	 * information assumes that objects would be placed into a hash table
+	 * with a certain number of slots (numSlots).
+	 * 
+	 * @param objects  An array of objects whose code information will be
+	 *                 printed.
+	 * @param numSlots The number of slots in the hypothetical hash table
+	 *                 where the objects would be placed.
+	 */
 	private static void printHashInfo(Object[] objects, int numSlots) {
 		System.out.println(
 				"Assuming " + numSlots + " slots in the hash table...");
@@ -13,20 +28,24 @@ public class HashCodes {
 	}
 
 	public static void main(String[] args) {
+		
+		////////////////////////////////////////////////////////////
+		//// Example 1: Integer, Double, String, Object (no collisions)
+		////////////////////////////////////////////////////////////
 		Object[] objects1 = { 5735, 5736, 27.50, 27.75, "a", "b", "aa",
 				"ab", new Object() };
 		int numSlots = 100;
-		System.out.println("Example 1:");
+		System.out.println("Example 1 (no collisions):");
 		printHashInfo(objects1, numSlots);
-		
-		Object[] objects2 = { "a", "m", "abf", "gggl",
-				"xyz" };
+
+		////////////////////////////////////////////////////////////
+		//// Example 1: Strings with collisions
+		////////////////////////////////////////////////////////////
+		Object[] objects2 = { "a", "m", "abf", "gggl", "xyz" };
 		numSlots = 20;
-		System.out.println("\n\nExample 2:");
+		System.out.println("\n\nExample 2 (with some collisions):");
 		printHashInfo(objects2, numSlots);
-		
-		
-		
+
 	}
 
 }
